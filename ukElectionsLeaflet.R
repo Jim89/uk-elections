5b87ef8f28daeec3ecac924bcf8d8c52be465ffd
Jim Leach <jim.leach@kpmg.co.uk>
2015-05-28 11:53:20 +0100

    cleaned up code and created a simply leaflet map to display resuts

--- ukElectionsLeaflet.R
+++ ukElectionsLeaflet.R
@@ -1,74 +1,69 @@
-###--------------------------------------------------
-### Scraping 2015 UK election data from the BBC
-###--------------------------------------------------
-
+################################################################################
+# Scraping and plotting 2015 UK election data
+################################################################################
 # set up working environment ---------------------------------------------------
-library(ggplot2)
-library(scales)
-library(dplyr)
-library(rvest)
-library(stringr)
-library(car)
-
-library(rgeos)
-library(maptools)
-library(rgdal)
-library(leaflet)
-
+# load packages
+  library(magrittr)
+  library(dplyr)
+  library(rvest)
+  library(stringr)
+  library(car)
+  library(rgeos)
+  library(rgdal)
+  library(leaflet)
+  library(readxl)
 
 # create functions to be used --------------------------------------------------
-
 # Convenience "not in" function
   "%nin%" <- function(x, y) {
     return( !(x %in% y) )
   }
 
 # get the results
-get.constituency.results <- function(constituency){
+  get.constituency.results <- function(constituency){
 
-  name <- constituency %>%
-          html_nodes(".constituency-title__title") %>%
-          html_text()
+    name <- constituency %>%
+            html_nodes(".constituency-title__title") %>%
+            html_text()
 
-  parties <- constituency %>%
-             html_nodes(".party__name--long") %>%
-             html_text()
+    parties <- constituency %>%
+               html_nodes(".party__name--long") %>%
+               html_text()
 
-  candidates <- constituency %>%
-                html_nodes(".party__result--candidate") %>%
-                html_text() %>%
-                str_replace(", with candidate ", "")
+    candidates <- constituency %>%
+                  html_nodes(".party__result--candidate") %>%
+                  html_text() %>%
+                  str_replace(", with candidate ", "")
 
-  votes <- constituency %>%
-           html_nodes(".party__result--votes") %>%
-           html_text() %>%
-           str_replace(",", "") %>%
-           str_replace(" total votes taken.", "") %>%
-           as.numeric()
+    votes <- constituency %>%
+             html_nodes(".party__result--votes") %>%
+             html_text() %>%
+             str_replace(",", "") %>%
+             str_replace(" total votes taken.", "") %>%
+             as.numeric()
 
-  vote.share <- constituency %>%
-                html_nodes(".party__result--votesshare") %>%
-                html_text() %>%
-                str_replace("% share of the total vote", "") %>%
+    vote.share <- constituency %>%
+                  html_nodes(".party__result--votesshare") %>%
+                  html_text() %>%
+                  str_replace("% share of the total vote", "") %>%
+                  as.numeric()
+
+    pct.swing <- constituency %>%
+                 html_nodes(".party__result--votesnet") %>%
+                 html_text() %>%
+                 str_replace("\\+", "") %>%
+                 str_replace("% change in share of the votes", "") %>%
                 as.numeric()
 
-  pct.swing <- constituency %>%
-               html_nodes(".party__result--votesnet") %>%
-               html_text() %>%
-               str_replace("\\+", "") %>%
-               str_replace("% change in share of the votes", "") %>%
-              as.numeric()
-
-  results.df <- data_frame(Constituency = name,
-                           Party = parties,
-                           Candidate = candidates,
-                           Votes = votes,
-                           Vote.Share = vote.share,
-                           Swing = pct.swing
-                           )
-  return(results.df)
+    results.df <- data_frame(Constituency = name,
+                             Party = parties,
+                             Candidate = candidates,
+                             Votes = votes,
+                             Vote.Share = vote.share,
+                             Swing = pct.swing
+                             )
+    return(results.df)
 }
-
 
 # get the data -----------------------------------------------------------------
 #  ff we've done all this already, don't do it again
@@ -99,7 +94,6 @@ if ( !file.exists("data/constituency_names.csv")) {
 
     write.csv(constituency_names.df, file="data/constituency_names.csv")
 }
-
 
 ###  And if we've done all *this* already, don't do it again
 if (!file.exists("data/uk-election-results-2015.csv")) {
@@ -150,7 +144,7 @@ if (!file.exists("data/uk-election-results-2015.csv")) {
 
 } else {
 
-# read in the data -------------------------------------------------------------
+# read in the elections results data -------------------------------------------
   data <- read.csv("data/uk-election-results-2015.csv", row.names=1)
   constituency_names.df <- read.csv("data/constituency_names.csv")
 
@@ -178,70 +172,116 @@ if (!file.exists("data/uk-election-results-2015.csv")) {
               group_by(Party) %>% summarize(Seats=n()) %>% arrange(desc(Seats))
 
 ## In Seat order
-by.seats <- data %>% group_by(Constituency) %>% filter(Votes==max(Votes)) %>%
-    group_by(Party) %>% tally() %>% arrange(desc(n)) %>%
-    data.frame(.)
+  by.seats <- data %>%
+              group_by(Constituency) %>%
+              filter(Votes==max(Votes)) %>%
+              group_by(Party) %>%
+              tally() %>%
+              arrange(desc(n)) %>%
+              data.frame(.)
 
-library(gdata)
-data$Party <- as.factor(data$Party)
-data$Party <- reorder.factor(data$Party, new.order=by.seats$Party)
-by.mps$Party <- reorder.factor(by.mps$Party, new.order=by.seats$Party)
-detach(package:gdata)
+# recode the factor levels for Party
+  library(gdata)
+  data$Party <- as.factor(data$Party)
+  data$Party <- reorder.factor(data$Party, new.order=by.seats$Party)
+  by.mps$Party <- reorder.factor(by.mps$Party, new.order=by.seats$Party)
+  detach(package:gdata)
 
+# set the party colours
+  uk.colors <- data.frame(Party=levels(by.mps$Party),
+                          party.color=c(
+                              "#1577C7", # Conservative
+                              "#E8251F", # Labour
+                              "#EAC135", # SNP
+                              "#BC1D40", # DUP
+                              "#FA8324", # Lim-Dems
+                              "#126140", # Sinn Fein
+                              "#559D37", # Plaid Cymru
+                              "#6AA769", # SDLP
+                              "#6EB2E4", # UUP
+                              "#7EC031", # Greens
+                              "#999999", # Independent
+                              "#6E3485" # UKIP
+                              ),
+                          stringsAsFactors = FALSE)
 
-###--------------------------------------------------
-### Now we can start looking at the data
-###--------------------------------------------------
-uk.colors <- data.frame(Party=levels(by.mps$Party),
-                        party.color=c(
-                            "#1577C7", # Conservative
-                            "#E8251F", # Labour
-                            "#EAC135", # SNP
-                            "#BC1D40", # DUP
-                            "#FA8324", # Lim-Dems
-                            "#126140", # Sinn Fein
-                            "#559D37", # Plaid Cymru
-                            "#6AA769", # SDLP
-                            "#6EB2E4", # UUP
-                            "#7EC031", # Greens
-                            "#999999", # Independent
-                            "#6E3485" # UKIP
-                            ),
-                        stringsAsFactors = FALSE)
-
-winners <- left_join(by.mps, constituency_names.df) %>%
-           left_join(uk.colors)
-library(gdata)
-winners$Party <- as.factor(winners$Party)
-winners$Party <- reorder.factor(winners$Party, new.order=by.seats$Party)
-detach(package:gdata)
-
-
-
-###--------------------------------------------------
-### Maps
-###--------------------------------------------------
-
-uk.map <- readOGR("maps/topo_wpc.json", "wpc")
-
-## The name field didn't get imported properly for some reason.
-# ind <- match(uk.map@data$id, constituency_names.df$id)
-ind <- match(uk.map@data$id, winners$id)
-uk.map@data$name <- winners$Constituency[ind]
-uk.map@data$party <- winners$Party[ind]
-uk.map@data$party.colour <- winners$party.color[ind]
-uk.map@data$mp <- winners$Candidate[ind]
-uk.map@data$popup <- paste0("<strong>",
-                            uk.map@data$name,
-                            "</strong>",
-                            "<br><strong>MP: </strong>",
-                            uk.map@data$mp)
+# read in the consituency economic data ----------------------------------------
+  wealth <- read_excel("./data/wealthDataExcel.xls", skip = 2, na = c(":")) %>%
+            setNames(c("Constituency", "id", "Prop.Over.65", "Pension.Claim.Rate",
+                       "Good.Health.Rate", "Prop.Non.White", "Median.House.Price",
+                       "Employment.Rate", "Unemployment.Rate", "Percentage.Tax.Credit",
+                       "Median.Income","Businesses.Per.10k"))
+  wealth <- wealth[1:650, ]
 
 
+# election results with wealth data (for ease of use later) --------------------
+  winners <- left_join(by.mps, constituency_names.df,
+                       by = c("Constituency" = "Constituency")) %>%
+             left_join(uk.colors, by = c("Party" = "Party")) %>%
+             left_join(wealth, by = c("id" = "id")) %>%
+             setNames(tolower(names(.))) %>%
+             select(constituency.x, party, candidate, votes, vote.share, swing,
+                    total.votes.cast, id, party.color, prop.over.65,
+                    pension.claim.rate, good.health.rate, prop.non.white,
+                    median.house.price, employment.rate, unemployment.rate,
+                    percentage.tax.credit, median.income, businesses.per.10k) %>%
+            rename(constituency = constituency.x) %>%
 
+# tidy up constituency to be a character not a factor
+  winners$constituency %<>% as.character
+
+
+# tidy up party to be a factor
+  library(gdata)
+  winners$party %<>% as.factor
+  winners$party %<>% reorder.factor(new.order = by.seats$Party)
+  detach(package:gdata)
+
+# clean up the crap we don't need
+  to_remove <- ls()
+  to_remove <- to_remove[-grep("winners", to_remove)]
+  rm(list = to_remove)
+  gc()
+
+
+# get and clean/combine the map data -------------------------------------------
+# get the map - doesn't have Northern Ireland, sadly :(
+  uk.map <- readOGR("maps/topo_wpc.json", "wpc")
+
+# find the overlaps in the id to perform the join
+  ind <- match(uk.map@data$id, winners$id)
+
+# bolt on bits of info from the "winners" data set that we might want to plot
+  uk.map@data$name <- winners$constituency[ind]
+  uk.map@data$party <- winners$party[ind]
+  uk.map@data$party.colour <- winners$party.color[ind]
+  uk.map@data$mp <- winners$candidate[ind]
+  uk.map@data$income <- winners$median.income[ind]
+  uk.map@data$prop.over.65 <- winners$prop.over.65[ind]
+  uk.map@data$businesses.per.10k <- winners$businesses.per.10k[ind]
+  uk.map@data$popup <- paste0("<strong>",
+                              uk.map@data$name,
+                              "</strong>",
+                              "<br><strong>MP: </strong>",
+                              uk.map@data$mp,
+                              "<br><strong>Median Income: </strong>&pound",
+                              uk.map@data$income,
+                              "<br><strong>Businesses per 10,000 people: </strong>",
+                              uk.map@data$businesses.per.10k)
+
+
+# create the leaflet plot ------------------------------------------------------
 leaflet(uk.map) %>%
-  addPolygons(stroke = F, fillOpacity = 0.95, smoothFactor = 0.5,
+  #addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(stroke = T,
+              color = "white",
+              weight = 0.3,
+              fillOpacity = 0.95,
+              smoothFactor = 0.5,
               fillColor = ~uk.map@data$party.colour,
-              popup = ~uk.map@data$popup)
-
+              popup = ~uk.map@data$popup) %>%
+  addLegend(position = "topleft",
+            colors = unique(uk.map@data$party.colour),
+            labels = unique(uk.map@data$party),
+            opacity = 0.95)
 
